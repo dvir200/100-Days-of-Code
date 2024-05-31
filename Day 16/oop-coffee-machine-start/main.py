33c144f9899a7628e2e4019a9a1fe3ec8710c6a0
dvir200 <76834454+dvir200@users.noreply.github.com>
2024-05-31 13:30:29 +0300

    Update main.py

--- Day 16/oop-coffee-machine-start/main.py
+++ Day 16/oop-coffee-machine-start/main.py
@@ -6,15 +6,6 @@ coffee_maker = CoffeeMaker()
 ourMenu = Menu()
 money_object = MoneyMachine()
 
-def operation(list_of_drinks, drink_wanted):
-  if drink_wanted == "latte":
-    user_drink = list_of_drinks.menu[0]
-  elif drink_wanted == "espresso":
-    user_drink = list_of_drinks.menu[1]
-  elif drink_wanted == "cappuccino":
-    user_drink = list_of_drinks.menu[2]
-  return user_drink
-
 machine_on = True
 while machine_on == True:
   user_drink = input(f"Choose one of the following items: {ourMenu.get_items()}: ")
@@ -22,11 +13,10 @@ while machine_on == True:
     machine_on = False
   elif user_drink == "report":
     coffee_maker.report()
+    money_object.report()
   else:
-    ourMenu.find_drink(user_drink)
-    user_drink = operation(ourMenu, user_drink)
-    if coffee_maker.is_resource_sufficient(user_drink) is True:
-      if money_object.make_payment(user_drink.cost) is True:
-        coffee_maker.make_coffee(user_drink)
-
-
+    if ourMenu.find_drink(user_drink) != None:
+      user_drink = ourMenu.find_drink(user_drink)
+      if coffee_maker.is_resource_sufficient(user_drink) is True:
+        if money_object.make_payment(user_drink.cost) is True:
+          coffee_maker.make_coffee(user_drink)
