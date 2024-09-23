1a002dc50f3a76e30c94b8fe8566cc7923a5b61d
dvir200 <76834454+dvir200@users.noreply.github.com>
2024-09-23 11:00:44 +0300

    Angela's version to pong added

---
+++ Day 22/Angela's Version/main.py
@@ -0,0 +1,31 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.title("Pong")
+screen.setup(width=800, height=600)
+screen.tracer(0)
+
+
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
+ball = Ball()
+
+
+screen.listen()
+screen.onkey(fun=r_paddle.go_up, key="Up")
+screen.onkey(fun=r_paddle.go_down, key="Down")
+screen.onkey(fun=l_paddle.go_up, key="w")
+screen.onkey(fun=l_paddle.go_down, key="s")
+
+game_is_on = True
+
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+screen.exitonclick()
