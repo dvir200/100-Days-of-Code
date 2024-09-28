77202c760e84a9739abfa5a51ce36bfd6d96b579
dvir200 <76834454+dvir200@users.noreply.github.com>
2024-09-28 18:37:02 +0300

    Update main.py

--- Day 24/Day Project/main.py
+++ Day 24/Day Project/main.py
@@ -14,10 +14,18 @@ with open("./Day 24/Day Project/Input/Names/invited_names.txt", "r") as file:
 with open("./Day 24/Day Project/Input/Letters/starting_letter.txt", "r") as file:
   list_letter = file.readlines()
 
-first_line_holder = list_letter[0] 
 
-name1 = names[0].strip("\n")
+for x in range(0, len(names)):
+  names[x] = names[x].strip("\n")
 
-new = first_line_holder.replace('[name]', name1)
-print(new)
 
+for x in range(0, len(names)):
+  print(names[x])
+  fileName = f"letter_for_{names[x]}.txt"
+  if x == 0:
+    list_letter[0] = list_letter[0].replace('[name]', names[x])
+  else:
+    list_letter[0] = list_letter[0].replace(names[x-1], names[x])
+  with open(f"./Day 24/Day Project/Output/ReadyToSend/{fileName}", "w") as file:
+    for line in list_letter:
+      file.write(line)
